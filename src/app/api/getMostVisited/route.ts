c81b0e339eb49d344443afe981ee79467b29ffb8
fiitr <162653860+fiitr@users.noreply.github.com>
2025-05-05 00:00:47 +0530

    Update route.ts

--- src/app/api/getMostVisited/route.ts
+++ src/app/api/getMostVisited/route.ts
@@ -3,22 +3,39 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
-  const mostVisited = await prisma.user.findMany({
-    orderBy: {
-      visits: "desc",
-    },
-    take: 50, // Fetch top 10 most visited users
-    select: {
-      EnrollmentNo: true,
-      Name: true,
-      visits: true,
-      StPic: true,
-    },
-  });
 
-  const mv = mostVisited.filter((user) => user.EnrollmentNo !== "22323004");
-
-  await prisma.$disconnect();
-  return NextResponse.json(mv);
+  try{
+    const mostVisited = await prisma.user.findMany({
+      orderBy: {
+        visits: "desc",
+      },
+      take: 50, // Fetch top 10 most visited users
+      select: {
+        EnrollmentNo: true,
+        Name: true,
+        visits: true,
+        StPic: true,
+      },
+      where: {
+        NOT: {
+          EnrollmentNo: "22323004"
+        }
+      }
+    });
+    
+    return NextResponse.json(mostVisited, {
+      status: 200,
+      headers: {
+        'Cache-Control': 'no-store, max-age=0',
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching most visited users:", error);
+    return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
